b3652d80562732ac0bb45ff1db4938e3a2e1237e
BorislavChernev <borocool359@gmail.com>
2022-09-12 16:30:34 +0300

    Completed Task

---
+++ Projects/Personal_Portfolio/js/script.js
@@ -0,0 +1,8 @@
+var hamburger = document.querySelector(".hamb");
+var navlist = document.querySelector(".nav-list");
+var links = document.querySelector(".nav-list li");
+
+hamburger.addEventListener("click", function(){
+    this.classList.toggle("click");
+    navlist.classList.toggle("open");
+})
